61bf1ebe3bd82c99541967a4be416cc60a7d287d
lupenza <lupenza10@gmail.com>
2025-07-23 23:31:11 +0300

    change migration

---
+++ database/migrations/2025_07_23_202910_change_column_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->longText('icon')->change();
+        });
+        Schema::table('min_services', function (Blueprint $table) {
+            $table->longText('icon')->change();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->string('icon')->change();
+        });
+
+        Schema::table('min_services', function (Blueprint $table) {
+            $table->string('icon')->change();
+        });
+    }
+};
